c19b802c6fe5414b361ac853fc29fafcb7b5f109
Thiago Zanatta <thiago@metadados.com.br>
2024-05-01 18:20:28 -0300

    Implementação do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia1 = new Nokia("5199991234", "G21", "351814800657339", 4);
+nokia1.Ligar();
+nokia1.InstalarAplicativo("Facebook");
+nokia1.ReceberLigacao();
+
+Smartphone iPhone1 = new Iphone("5199991235", "G21", "351814800657339", 4);
+iPhone1.Ligar();
+iPhone1.InstalarAplicativo("Facebook");
+iPhone1.ReceberLigacao();
